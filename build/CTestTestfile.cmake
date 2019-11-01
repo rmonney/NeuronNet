710da65f15a3ffc43c4b312e8e4259fa91981f47
Monney Rachel <rmonney@INTRANET.EPFL.CH@SVUbuUniv23.intranet.epfl.ch>
2019-11-01 12:19:16 +0100

    retrait build et doc

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/rmonney/myfiles/Programmation/cpp/BA3/NeuronNet
-# Build directory: /home/rmonney/myfiles/Programmation/cpp/BA3/NeuronNet/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(CMake_random "testNeuron")
